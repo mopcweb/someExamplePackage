f9378b90fa07df5dfd370a0ef05f9d0457b2b218
Danil Moroz <dmoroz@signet.tv>
2020-09-09 21:44:52 +0300

    Git init

---
+++ packages/tsed/src/utils/config.ts
@@ -0,0 +1,12 @@
+import { Configuration } from '@tsed/common';
+
+export const defaultConfig: Partial<Configuration> = {
+  // port: 3000,
+  // rootDir: __dirname,
+  // commonServerDir: __dirname,
+  // httpsPort: false,
+  // routers: { mergeParams: true },
+  // logger: { level: 'off' },
+  // logger: { logRequest: false, logEnd: false, logStart: false },
+  // exclude: ['**/*.spec.ts', '**/*.d.ts'],
+};
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Steps for running in watch mode
+
+```bash
+# Install deps
+yarn
+
+# Run in watch mode
+yarn watch
+```
